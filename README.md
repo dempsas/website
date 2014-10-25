c1192058781011735fc9b9ca094eab13bcb50636
Adam <dempsas@gmail.com>
2014-10-25 20:26:20 +1300

    Cleaned up more CSS, cropped images to 16x9, removed parralax annimation, got rid of header nav on mobile version

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ##Simple one page layout with a fixed header for jquery navigation
 
 Colors and content and easily be swapped for template to be reused.
+
 ========
--- script.js
+++ script.js
@@ -1,53 +1,39 @@
 /*
  * Some jQuery for website niceness
- * Parallax header, Mobile nav and one page scroll navigation. 
+ * Mobile nav and one page scroll navigation.
  */
-$(document).ready(function() {
-    if ($(window).width() > 768) {
-        $('div[data-type="background"]').each(function() {
-            var $bgobj = $(this); // assigning the object
-
-            $(window).scroll(function() {
-                var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
-
-                // Put together our final background position
-                var coords = '50% ' + yPos + 'px';
-
-                // Move the background
-                $bgobj.css({backgroundPosition: coords});
-            });
-        });
-    }
-
-    $('a[href^="#"]').on('click', function(e) {
+$(document).ready(function () {
+    $("#logo").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
 
         var target = this.hash,
-                $target = $(target);
+            $target = $(target);
 
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
-        }, 900, 'swing', function() {
+        }, 900, 'swing', function () {
             window.location.hash = target;
         });
     });
     if ($(window).width() < 768) {
-        $('.nav').click(function() {
+        $('.nav').click(function () {
             $("#headerContainer").slideUp('slow');
         });
-    }
-    else {
-    }
+    } else {}
     $('#welcome').delay('500').fadeIn('3000');
-    $("#L").click(function() {
+    $("#L").click(function () {
         if ($("#headerContainer").is(":hidden")) {
             $("#headerContainer").slideDown("slow");
         } else {
             $("#headerContainer").slideUp('slow');
         }
     });
-    
-    $(window).load(function() {
-    $('.flexslider').flexslider();
-  });
+    $(window).load(function () {
+        $('.flexslider').flexslider();
+    });
 });
